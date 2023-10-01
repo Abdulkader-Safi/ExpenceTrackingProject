bed2163811bc1a96b4470d964337809534f24fed
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-01 14:59:31 +0300

    work on model2

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,24 @@ import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
+enum Category {
+  food,
+  travel,
+  leisure,
+  word,
+}
+
 class Expense {
   final String id;
   final String title;
   final String amount;
   final DateTime date;
+  final Category category;
 
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4();
 }
