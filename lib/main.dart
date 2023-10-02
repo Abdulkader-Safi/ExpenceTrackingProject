1436b6927a860d8cc46670b6f392d83f4ef1d887
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-02 21:48:16 +0300

    working on main application theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ void main() {
   runApp(const MyApp());
 }
 
+var myColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 59, 96, 179),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -13,8 +17,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: ThemeData().copyWith(
         useMaterial3: true,
+        colorScheme: myColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: myColorScheme.onPrimaryContainer,
+          foregroundColor: myColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: myColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: myColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: myColorScheme.onSecondaryContainer,
+                fontSize: 17,
+              ),
+            ),
       ),
       home: const Expenses(),
     );
