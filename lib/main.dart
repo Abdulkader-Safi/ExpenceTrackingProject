087bc8229f3c68efd3da316003f22b017aa41bab
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-02 22:09:07 +0300

    working on main application theme 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ var myColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 59, 96, 179),
 );
 
+var myDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 59, 96, 179),
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -17,9 +21,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: myColorScheme,
+        bottomSheetTheme: const BottomSheetThemeData().copyWith(
+          backgroundColor: myDarkColorScheme.primaryContainer,
+        ),
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: myColorScheme.onPrimaryContainer,
           foregroundColor: myColorScheme.primaryContainer,
@@ -30,13 +38,42 @@ class MyApp extends StatelessWidget {
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: myColorScheme.primaryContainer,
+            backgroundColor: myDarkColorScheme.onPrimaryContainer,
+            foregroundColor: myDarkColorScheme.primaryContainer,
           ),
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
                 fontWeight: FontWeight.normal,
                 color: myColorScheme.onSecondaryContainer,
+                fontSize: 17,
+              ),
+            ),
+      ),
+      darkTheme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: myDarkColorScheme,
+        bottomSheetTheme: const BottomSheetThemeData().copyWith(
+          backgroundColor: myDarkColorScheme.primaryContainer,
+        ),
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: myDarkColorScheme.onPrimaryContainer,
+          foregroundColor: myDarkColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: myDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: myDarkColorScheme.onPrimaryContainer,
+            foregroundColor: myDarkColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: myDarkColorScheme.onSecondaryContainer,
                 fontSize: 17,
               ),
             ),
